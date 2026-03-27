a5534f04e757979b8afcfcda569d76d3cba3b3fd
sheremet <Bobrovmiha18@gmail.com>
2026-03-28 01:14:48 +0200

    Update 01-color-switcher.js

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -3,7 +3,6 @@ const stopBtn = document.querySelector('[data-stop]');
 const bodyEl = document.body;
 
 let timerId = null;
-
 stopBtn.disabled = true;
 
 function getRandomHexColor() {
@@ -16,17 +15,16 @@ startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
   stopBtn.disabled = false;
 
-  bodyEl.style.backgroundColor = getRandomHexColor();
+  bodyEl.style.background = getRandomHexColor();
 
   timerId = setInterval(() => {
-    bodyEl.style.backgroundColor = getRandomHexColor();
+    bodyEl.style.background = getRandomHexColor();
   }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   timerId = null;
-
   startBtn.disabled = false;
   stopBtn.disabled = true;
 });
